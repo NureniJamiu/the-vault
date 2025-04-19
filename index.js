552fa52bf7e7600e3d77a1817b4fc4a2f732bb29
NureniJamiu <nurenijamiu951@gmail.com>
2025-04-19 15:21:48 -0700

    create POST intel route

--- index.js
+++ index.js
@@ -2,10 +2,40 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+app.post("/api/intel", async (req, res) => {
+  // const title = req.body.title;
+  // const description = req.body.description;
+
+  try {
+    // destructuring the request body to get title and description
+    const { title, description } = req.body;
+
+    if (!title || !description) {
+      return res.status(400).json({
+        message: "Title and description are required",
+      });
+    }
+
+    console.log(title, description);
+
+    res.status(201).json({
+      message: "Intel created successfully",
+      intel: {
+        title,
+        description,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error creating intel",
+    });
+  }
 });
 
+// app is listening on port 3000 and logs a message to the console
 app.listen(3000, () => {
   console.log("Server running");
 });
